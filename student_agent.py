0e547032377b0368d036522a412817e67b170c9e
gcobs104628 <gcobs104628@gmail.com>
2025-03-13 15:42:03 +0800

    add baseline to leaderboard

--- student_agent.py
+++ student_agent.py
@@ -3,6 +3,7 @@ import numpy as np
 import pickle
 import random
 import gym
+import torch
 
 def get_action(obs):
     
@@ -11,8 +12,27 @@ def get_action(obs):
     # NOTE: Keep in mind that your Q-table may not cover all possible states in the testing environment.
     #       To prevent crashes, implement a fallback strategy for missing keys. 
     #       Otherwise, even if your agent performs well in training, it may fail during testing.
+    
+    action = [0,1, 2, 3, 4, 5]
+    if random.uniform(0, 1) < 0.25:
+        return random.choice(action)
+    else:
+        if obs[10]==1:
+            action.remove(1)  
+        if obs[13]==1:
+            action.remove(3)  
+        if obs[12]==1:
+            action.remove(2)
+        if obs[11]==1:
+            action.remove(0)
+        if obs[14]!=1:
+            action.remove(4)
+        if obs[15]!=1:
+            action.remove(5)
+        return random.choice(action)
+            
+    #return random.choice(action) # Choose a random action
 
-
-    return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
+    #return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
     # You can submit this random agent to evaluate the performance of a purely random strategy.
 
